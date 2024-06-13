164c0d0438cf5e8ef7133e23b114ad4e942a80ce
shining-jung <tjddnjz51w@gmail.com>
2024-06-13 15:25:12 +0900

    job 온라인/오프라인 get 요청

--- job.js
+++ job.js
@@ -19,11 +19,36 @@ router.post("/jobWrit", async (req, res) => {
       status: 1,
       category,
     });
-    console.log(jobPostDoc);
     res.json(jobPostDoc);
   } catch (e) {
     console.error("Error creating document:", e.message);
     res.status(400).json({ message: "failed", error: e.message });
   }
 });
+
+router.get("/jobOffer", async (req, res) => {
+  try {
+    const jobList = await JobPost.find().sort({ createdAt: -1 }).limit(8);
+    res.json(jobList);
+  } catch (e) {
+    res.json({ message: "server(500) error" });
+  }
+});
+router.get("/findonLine", async (req, res) => {
+  try {
+    const jobList = await JobPost.find({ "category.jobType": "onLine" }).sort({ createdAt: -1 }).limit(8);
+    res.json(jobList);
+  } catch (e) {
+    res.json({ message: "server(500) error" });
+  }
+});
+router.get("/findoffLine", async (req, res) => {
+  try {
+    const jobList = await JobPost.find({ "category.jobType": "offLine" }).sort({ createdAt: -1 }).limit(8);
+    res.json(jobList);
+  } catch (e) {
+    res.json({ message: "server(500) error" });
+  }
+});
+
 module.exports = router;
