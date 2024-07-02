a172892e0bfd0fd178b0d5f1b34eb679784a1c3d
chaerin-kim <chery5809@naver.com>
2024-07-02 09:13:37 +0900

    [만족도 조사] 스키마 수정

--- modules/Satisfied.js
+++ modules/Satisfied.js
@@ -3,18 +3,24 @@ const mongoose = require('mongoose');
 const satisfiedSchema = new mongoose.Schema(
   {
     Post_id: { type: String, required: true },
-    repondentID: { type: String, required: true }, // 조사 쓴 사람 이메일
-    repondentNick: { type: String, required: true },
+    respondentID: { type: String, required: true }, // 조사 쓴 사람 이메일
+    respondentNick: { type: String, required: true },
+    recipientID: { type: String, required: true },
     matchedID: { type: String, required: true },
     writerID: { type: String, required: true },
-    starRating: { type: Number, required: true },
-    kind: { type: Number, required: true },
-    onTime: { type: Number, required: true },
-    highQuality: { type: Number, required: true },
-    unkind: { type: Number, required: true },
-    notOnTime: { type: Number, required: true },
-    lowQuality: { type: Number, required: true },
-    etc: { type: Number, required: true },
+    starRating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    kind: { type: Number, required: true, default: 0 },
+    onTime: { type: Number, required: true, default: 0 },
+    highQuality: { type: Number, required: true, default: 0 },
+    unkind: { type: Number, required: true, default: 0 },
+    notOnTime: { type: Number, required: true, default: 0 },
+    lowQuality: { type: Number, required: true, default: 0 },
+    etc: { type: Number, required: true, default: 0 },
     etcDescription: { type: String, default: '' },
   },
   { timestamps: true }
